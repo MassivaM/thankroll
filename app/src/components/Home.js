9e0881dc4a1a1150cc4aae610dc798bba0ca139b
MassivaM <hamlimassy@hotmail.com>
2020-06-28 23:09:21 -0400

    initial commit

---
+++ app/src/components/Home.js
@@ -0,0 +1,17 @@
+import React , {Component} from 'react'
+
+
+export default class Home extends Component{
+
+
+    render(){
+        return (
+            <div className="Home">
+              <h1>Thankloop</h1>
+            </div>
+          );
+
+    }
+    
+
+}
