816bba41d16d47efdb236c11b6e2ac58e5a931e9
MassivaM <hamlimassy@hotmail.com>
2020-07-16 00:09:21 -0400

    loading circle added to home page

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Home from './components/Home'
-
-import './App.scss';
+import './sass/base.scss'
 
 function App() {
   return (
     <div className="App">
      <Home/>
     </div>
+
   );
 }
 
--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,15 +1,52 @@
 import React , {Component} from 'react'
-
+import title from '../assets/thankloop-title.svg'
 
 export default class Home extends Component{
 
-
+//
     render(){
+      let i 
         return (
-            <div className="Home">
-              <h1>Thankloop</h1>
+
+          <body>
+
+            
+           
+                
+                <div className= "box-loading"> 
+
+                
+          
+                <div className= "cirlce-wave"> 
+
+               
+               
+                  
+               </div>
+               <div className="cirlce-wave--2">
+     
+              </div>
+              <div className="cirlce-wave--3">
+     
+              </div>
+              <div className="cirlce-wave--4">
+     
+              </div>
+              <div className="cirlce-wave--5">
+              
+              </div>
+             
+              <div className="text">
+      
+                <img src={title} style={{ height: 500, width: 700 }}
+              alt="website logo" />
+              </div>
+        
             </div>
-          );
+
+            
+          </body>
+          )
 
     }
     
