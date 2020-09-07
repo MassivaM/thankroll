76be7da3ea94aea75bff2a25786266cef9442e5d
MassivaM <hamlimassy@hotmail.com>
2020-09-07 01:26:47 -0400

    swipper ready

--- app/src/components/Swipper.js
+++ app/src/components/Swipper.js
@@ -1,20 +1,50 @@
-import React , {Component} from 'react'
+import React,{Component} from 'react'
+import thankloop from '../assets/thankloop-white-logo.svg'
+import rightarrow from '../assets/right-arrow.png'
+import 'swiper/swiper-bundle.css';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import Swiper, { Navigation, Pagination } from 'swiper';
+import fire from '../assets/fire.jpg'
+// configure Swiper to use modules
+Swiper.use([Navigation, Pagination]);
 
-
+// init Swiper:
 
 export default class Swipper extends Component{
+ 
+render(){
+
+   
+  return(
+   
+	
+		<div className="card">
+			<div className="left">
+			<div className="ava"><img src={fire}/></div>
+				<h1 className="name">	Nick Canon</h1>
+        
+				<span className="status">Firefighter</span>
+  
+			</div>
+			<div className="right">
+			<span className="descr">
+				Nick is a firefighter and has started a new initiative for young boys and girls to come visit the fire station every week to teach how to protect themselves and their family.
+				</span>
+				<button className="follow_btn">
+				<img src={thankloop} alt="Place Holder" />
+				
+				<span>Thank Nick</span>
+				</button>
+				
+			</div>
+			<button className="next">
+			<img src={rightarrow}/>
+		
+				</button>
+		</div>
 
 
-    render(){
-      let i 
-        return (
-            <div className="box">
-
-
-            </div>
-        )
-
-    }
-    
-
+   
+  )
+}
 }
--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -8,53 +8,17 @@ import Intro from '../components/Intro.js'
 import Person from '../components/Person.js';
 import data from '../data.json';
 
-const Home = () => {
+export default class Home extends Component {
 
-  const [people, setPeople] = useState(data);
-  const [likedUsers, setLikedUsers] = useState([]);
-  const [dislikedUsers, setDislikedUsers] = useState([]);
-  const activeUser = 0;
-
-  const removedPersonFromDataSrc = (peopleSource, userId) =>
-    peopleSource.filter(user => user.id !== userId);
-
-  const modifySuperficialChoices = (userId, action) => {
-    const newPeople = [...people];
-    const newLikedUsers = [...likedUsers];
-    const newDislikedUsers = [...dislikedUsers];
-
-    switch (action) {
-      case 'ADD_TO_LIKED_USERS':
-        if (!people[activeUser].likedUsers.includes(userId)) {
-          newPeople[activeUser].likedUsers.push(userId);
-          newLikedUsers.push(data[userId]);
-
-          setLikedUsers(newLikedUsers);
-          setPeople(removedPersonFromDataSrc(people, userId));
-        }
-        break;
-      case 'ADD_TO_DISLIKED_USERS':
-        if (!people[activeUser].dislikedUsers.includes(userId)) {
-          newPeople[activeUser].dislikedUsers.push(userId);
-          newDislikedUsers.push(data[userId]);
-
-          setDislikedUsers(newLikedUsers);
-          setPeople(removedPersonFromDataSrc(people, userId));
-        }
-        break;
-     
-      default:
-        return people;
-    }
-  };
-   
+  
+   render(){
         return (
 
           <body>
-<Intro/>
-
-
-
+           
+          <Intro visible={this.visible}/>
+          
+       
             
           </body>
           )
@@ -63,5 +27,4 @@ const Home = () => {
     
 
 }
-
-export default Home
+}
