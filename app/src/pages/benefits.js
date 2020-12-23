39ce49a6bb6f82b7e039bb761e01376bbd65b49f
MassivaM <hamlimassy@hotmail.com>
2020-12-22 21:19:55 -0500

    benefits of thanking new design

--- app/src/pages/benefits.js
+++ app/src/pages/benefits.js
@@ -17,37 +17,28 @@ export default class Submit extends Component {
         <div className="welcome">
           <div className="Rectangle">
             <div className="Rectangle2">
-              <h1>Why bother thanking?</h1>
+              <h1>It's about more than just saying thanks.</h1>
             </div>
           </div>
-
-          <p className="article">
-            We’re on a mission to have people thank each other more often, but
-            that’s not just because we want a kinder world: there are{" "}
-            <span style={{ fontWeight: 700, color: "#6c5ce7" }}>
-              {" "}
-              scientifically proven benefits to thanking and gratitude, for
-              others and for you as well.{" "}
-            </span>
-            <br></br>
-            <br></br>
-            For years, research has shown gratitude not only reduces stress, but
-            it may also play a major role in overcoming trauma.  Gratitude
-            reduces a multitude of toxic emotions, ranging from envy and
-            resentment to frustration and regret. It effectively increases
-            happiness and reduces depression (Robert Emmonds). In addition, a
-            2006 study published in Behavior Research and Therapy found that
-            Vietnam War Veterans with higher levels of gratitude experienced
-            lower rates of Post-Traumatic Stress Disorder.  
-            <br></br>
-            <br></br>
-            So, when you go and thank people on this website, you create a key
-            moment of happiness for the other person, but for you as well.
-            <br></br>
-            <br></br>
-            We rely entirely on donations; if you want to help us keep this site
-            alive, click on the donation below.
-          </p>
+        </div>
+        <div style={{ width: "100%" }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 365.38"
+            style={{
+              width: "100%",
+              height: "auto",
+              top: 450,
+              margin: 0,
+              left: 0,
+            }}
+          >
+            <path
+              fill="#6C5CE7"
+              fill-opacity="1"
+              d="M0,160L26.7,170.7C53.3,181,107,203,160,224C213.3,245,267,267,320,266.7C373.3,267,427,245,480,218.7C533.3,192,587,160,640,165.3C693.3,171,747,213,800,202.7C853.3,192,907,128,960,122.7C1013.3,117,1067,171,1120,213.3C1173.3,256,1227,288,1280,288C1333.3,288,1387,256,1413,240L1440,224L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"
+            ></path>
+          </svg>
         </div>
       </div>
     );
