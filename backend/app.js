39257d2f2ddfdbad2643742b9eebdb3b0b96865f
MassivaM <hamlimassy@hotmail.com>
2020-12-31 13:55:53 -0500

    schema and resolver

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.send("Hello World");
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+  type RootQuery{
+      profiles: [String!]!
+  }
+  type RootMutation{
+      createProfile(name: String): String
+  }
+  schema {
+    query:RootQuery
+    mutation: RootMutation
+  }
+  `),
+    rootValue: {
+      profiles: () => {
+        //return list of profiles
+        return ["Marie-Ève Ferron", "Olivier Chartrand", "Ayla Mahamli"];
+      },
+      createProfile: (args) => {
+        const profileName = args.name;
+        return profileName;
+      },
+    },
+    graphiql: true,
+  })
+);
 //host for the page
 app.listen(4000);
