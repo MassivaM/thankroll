fa87573a2a285298ebb3755dda479da1cc84ad95
MassivaM <hamlimassy@hotmail.com>
2021-01-02 00:50:08 -0500

    connected to mongodb

--- backend/app.js
+++ backend/app.js
@@ -2,20 +2,40 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
-
+const mongoose = require("mongoose");
 const app = express();
 
-app.use(bodyParser.json());
+const Profile = require("./models/profiles");
 
+app.use(bodyParser.json());
+//created a file type to describe the pictures uploaded
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: buildSchema(`
+
+    type Profile{
+      _id: ID!
+      name: String!
+      description: String!
+      profession: String!
+      picture: String!
+      date: String!
+     
+    }
+   
+    input ProfileInput{
+      name: String!
+      description: String!
+      profession: String!
+      picture: String!
+      date: String!
+    }
   type RootQuery{
-      profiles: [String!]!
+      profiles: [Profile!]!
   }
   type RootMutation{
-      createProfile(name: String): String
+      createProfile(profileinput: ProfileInput): Profile
   }
   schema {
     query:RootQuery
@@ -24,16 +44,47 @@ app.use(
   `),
     rootValue: {
       profiles: () => {
-        //return list of profiles
-        return ["Marie-Ève Ferron", "Olivier Chartrand", "Ayla Mahamli"];
+        //return all entries in collection
+        return Profile.find()
+          .then((profiles) => {
+            return profiles.map((profile) => {
+              return { ...profile._doc };
+            });
+          })
+          .catch((err) => {
+            throw err;
+          });
       },
       createProfile: (args) => {
-        const profileName = args.name;
-        return profileName;
+        const profile = new Profile({
+          name: args.profileinput.name,
+          description: args.profileinput.description,
+          profession: args.profileinput.profession,
+          picture: args.profileinput.picture,
+          date: new Date(args.profileinput.date),
+        });
+        return profile
+          .save()
+          .then((result) => {
+            console.log(result);
+            return { ...result._doc };
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
       },
     },
     graphiql: true,
   })
 );
-//host for the page
-app.listen(4000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.vcfwq.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(4000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ backend/models/profiles.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const profileSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  profession: {
+    type: String,
+    required: true,
+  },
+  picture: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Profile", profileSchema);
